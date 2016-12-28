000d7a8cdbc829ff28a2772fd3cd815cac0e47ef
Fritz Grimpen <fritz@grimpen.net>
2016-12-28 05:13:18 +0000

    First draft for YoloCloud tool

--- yolocloud/tool.py
+++ yolocloud/tool.py
@@ -3,10 +3,27 @@
 
 import argparse
 
+import libvirt
+import sqlalchemy
+import sqlalchemy.orm
+
 import yolocloud.database as database
 import yolocloud.virt as virt
 
-def create_token():
+Session = sqlalchemy.orm.sessionmaker()
+
+def main_create_token(args):
+    session = Session()
+    token = database.Token(token=args.token, libvirt_url=args.libvirt_url,
+            regenerates=args.regenerate, vm_lifetime=args.vm_lifetime)
+    session.add(token)
+    session.commit()
+
+def main_create_domain(args):
+    session = Session()
+    vm = database.VirtualMachine()
+
+def main_delete_domain(args):
     pass
 
 def main(argv):
@@ -20,6 +37,7 @@ def main(argv):
 
     argparser_create_token = subparsers.add_parser("create-token")
     argparser_create_domain = subparsers.add_parser("create-domain")
+    argparser_delete_domain = subparsers.add_parser("delete-domain")
 
     argparser_create_token.add_argument("--vm-lifetime", type=int, default=0,
             dest="vm_lifetime", help="Lifetime for virtual machines in SI seconds")
@@ -28,14 +46,27 @@ def main(argv):
     argparser_create_token.add_argument("--libvirt-url", type=str,
             dest="libvirt_url", help="Enforce libvirt URL for generated virtual machines")
     argparser_create_token.add_argument("token", nargs="?", type=str,
-            default="", help="Token string")
+            default=None, help="Token string")
+
+    argparser_create_domain.add_argument("template", type=str,
+            help="Domain template name")
+    argparser_create_domain.add_argument("--provision", type=bool, default=True,
+            dest="provision", help="If set, enqueue provisioning")
+    
+    argparser_delete_domain.add_argument("--keep-hdd", type=bool, default=False,
+            dest="keep_hdd", help="If set, then the HDDs won't get deleted.")
 
     args = argparser.parse_args(argv)
 
+    engine = sqlalchemy.create_engine(args.engine)
+    Session.configure(bind=engine)
+
     if args.command == "create-token":
-        pass
-    elif args.command == "create-domain"):
-        pass
+        main_create_token(args)
+    elif args.command == "create-domain":
+        main_create_domain(args)
+    elif args.command == "delete-domain":
+        main_delete_domain(args)
 
 if __name__ == "__main__":
     import sys
